b69ffd3b84e1aa4891c90b263fd4ae02839ae931
hannington owiti <hanningtonowiti@gmail.com>
2024-06-29 09:10:58 +0300

    arrowFunctions

---
+++ index.js
@@ -0,0 +1,11 @@
+//function expression called divide
+const divide = function(x,y)
+{
+    return 2000/100;
+}
+
+//arrow square
+const square = num => {return num *num}
+
+//arrow add
+const add = (a,b) => {return a + b}
